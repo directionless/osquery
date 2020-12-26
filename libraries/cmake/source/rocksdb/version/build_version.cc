722536d9c310cf4e951994956618f5fe1ac253a6
seph <seph@directionless.org>
2020-12-25 22:46:02 -0500

    rocks

--- libraries/cmake/source/rocksdb/version/build_version.cc
+++ libraries/cmake/source/rocksdb/version/build_version.cc
@@ -1,5 +1,5 @@
 // Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved.
 #include "build_version.h"
-const char* rocksdb_build_git_sha = "rocksdb_build_git_sha:@35d8e36ef1b8e3e0759ca81215f855226a0a54bd@";
-const char* rocksdb_build_git_date = "rocksdb_build_git_date:@2020/11/20 13:57:55@";
+const char* rocksdb_build_git_sha = "rocksdb_build_git_sha:@601585bca4ede0af6b9e52da2d4468ce37556aaf@";
+const char* rocksdb_build_git_date = "rocksdb_build_git_date:@2020/12/25 22:45:55@";
 const char* rocksdb_build_compile_date = __DATE__;
